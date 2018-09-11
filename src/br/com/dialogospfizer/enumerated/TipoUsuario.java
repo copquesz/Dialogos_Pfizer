a233350ab094ca50b670e105666e5ddb8ecdb26f
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-11 15:37:19 -0300

    adicionando o Enum responsável pelo tipos de usuários da aplicação.

---
+++ src/br/com/dialogospfizer/enumerated/TipoUsuario.java
@@ -0,0 +1,27 @@
+package br.com.dialogospfizer.enumerated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+/**
+ * Enum que contém os tipos de Usuários da aplicação.
+ * @author Yabá
+ *
+ */
+public enum TipoUsuario{
+	
+	COMUM(0, "Usuário Comum"),
+	YABA(1, "Usuário Yabá"),
+	PFIZER(2, "Usuário Pfizer");
+	
+	private Integer id;
+	private String descricao;
+	
+	TipoUsuario(Integer id, String descricao){
+		this.id = id;
+		this.descricao = descricao;
+		
+	}
+
+}
