c4d1c453256f7cb41f9206e8ab8ae29dfd07911a
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-11 17:01:15 -0300

    configurando as regras de negocio para o método de adicionar do objeto Usuario.

--- src/br/com/dialogospfizer/service/UsuarioService.java
+++ src/br/com/dialogospfizer/service/UsuarioService.java
@@ -1,16 +1,20 @@
 package br.com.dialogospfizer.service;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import br.com.dialogospfizer.dao.UsuarioDAO;
 import br.com.dialogospfizer.entity.Usuario;
+import br.com.dialogospfizer.enumerated.TipoUsuario;
 
 @Service
 public class UsuarioService {
 	
 	private UsuarioDAO dao;
 	
-	public Usuario adicionar (Usuario usuario) {
+	public Usuario adicionar (Usuario usuario, TipoUsuario tipoUsuario) {
+		usuario.setDataCadastro(new Date());
 		return dao.adicionar(usuario);
 	}
 
