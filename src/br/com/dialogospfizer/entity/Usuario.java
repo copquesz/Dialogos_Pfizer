9ef065bd94a864b5c4595f326fb7aa40f8c15b33
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-11 15:20:55 -0300

    adicionando a classe modelo USUÁRIO

---
+++ src/br/com/dialogospfizer/entity/Usuario.java
@@ -0,0 +1,54 @@
+package br.com.dialogospfizer.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name = "usuario")
+/**
+ * Classe Entidade responsável por possui o modelo do objeto Usuário
+ * @author Lucas Copque
+ *
+ */
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 2357710625085133022L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data_cadastro")
+	private Date dataCadastro;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "sobrenome")
+	private String sobrenome;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "senha")
+	private String senha;
+
+}
