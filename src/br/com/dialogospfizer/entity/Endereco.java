1658b580c96c31acef4eda40793de7ed09d5a8a7
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-11 15:25:23 -0300

    adicionando a classe modelo ENDERECO

---
+++ src/br/com/dialogospfizer/entity/Endereco.java
@@ -0,0 +1,57 @@
+package br.com.dialogospfizer.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name = "endereco")
+/**
+ * Classe responsável por possui o modelo do objeto Endereço
+ * @author Lucas Copque
+ *
+ */
+public class Endereco implements Serializable{
+	
+	private static final long serialVersionUID = -3881872537152516919L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "logradouro")
+	private String logradouro;
+	
+	@Column(name = "numero")
+	private String numero;
+	
+	@Column(name = "complemento")
+	private String complemento;
+	
+	@Column(name = "cep")
+	private String cep;
+	
+	@Column(name = "bairro")
+	private String bairro;
+	
+	@Column(name = "cidade")
+	private String cidade;
+	
+	@Column(name = "estado")
+	private String estado;
+
+}
