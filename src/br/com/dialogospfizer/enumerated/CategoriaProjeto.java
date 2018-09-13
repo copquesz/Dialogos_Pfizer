fc91226c4fb792d7f455564cd941645d3a7d85d5
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-13 14:24:56 -0300

    adicionando o enum para categoria dos projetos.

---
+++ src/br/com/dialogospfizer/enumerated/CategoriaProjeto.java
@@ -0,0 +1,17 @@
+package br.com.dialogospfizer.enumerated;
+
+public enum CategoriaProjeto {
+	
+	PSIORIASE(0, "Psioriase"),
+	DOENCAS_REUMATICAS(1, "Doenças Reumáticas"),
+	DOENCAS_INFLAMATORIAS(2, "Doenças Inflamatórias");
+	
+	private int id;
+	private String descricao;
+	
+	CategoriaProjeto(int id, String descricao){
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+}
